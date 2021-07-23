c73cf6999e43c25fb6c602ed6cfb23e45e81e028
rlals8145 <rlals8145>
2021-07-23 17:38:19 +0900

    안드로이드 앱 커밋

---
+++ app/src/main/java/kr/or/myapplication0723/MainActivity.java
@@ -0,0 +1,65 @@
+package kr.or.myapplication0723;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigation;
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+    Fragment4 fragment4;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navigation = findViewById(R.id.navigation);
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+        fragment4 = new Fragment4();
+
+
+
+        //프래그먼트매니저를 이용하여 FrameLayout에 Fragment화면을 설정
+        //replace(부모 레이아웃, 화면에 출력할 프래그먼트객체)
+
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.item_home){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment1).commit();
+                }else if (itemId == R.id.item_camera){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment2).commit();
+                }else if (itemId == R.id.item_news){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment3).commit();
+                }else {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment4).commit();
+                }
+
+
+                //true : 클릭한 메뉴아이템에 포커싱
+                return true;
+            }
+        });
+
+
+    }
+}
